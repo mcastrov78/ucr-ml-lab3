674c91b3075a51257090bafa83051ad02de92059
Mauricio Castro <mauricio.castro@avantica.net>
2020-05-19 11:12:18 -0600

    Validating against test set.

--- mnist_digits.py
+++ mnist_digits.py
@@ -69,41 +69,65 @@ def train_nn(iterations, nn_model, optimizer, nn_loss_fn, tensor_x, tensor_y, in
 
 def main(trainingdataf="train-images.idx3-ubyte", traininglabelf="train-labels.idx1-ubyte",
          testdataf="t10k-images.idx3-ubyte", testlabelf="t10k-labels.idx1-ubyte"):
-    data, data_dims = read_idx.read(trainingdataf, 50)
-    labels, labels_dims = read_idx.read(traininglabelf, 50)
+    # --------------- TRAINING ---------------
+    # read training data
+    train_data, train_data_dims = read_idx.read(trainingdataf, 50)
+    train_labels, train_label_dims = read_idx.read(traininglabelf, 50)
 
-    # Convert tensors to the appropriate data types, and - in the case of the images - shape
-    print("\ndata (%s): %s" % (len(data), data))
-    training_data = torch.tensor(data, dtype=torch.float)
-    training_data = training_data.view((-1, 28 * 28))
-    training_data /= 255
-    print("training_data (%s): %s" % (training_data.size(), training_data))
+    # convert tensors to the appropriate data types, also shape and normalize images data
+    train_data = torch.tensor(train_data, dtype=torch.float)
+    train_data = train_data.view((-1, 28 * 28))
+    train_data /= 255
+    print("train_data (%s): %s" % (train_data.size(), train_data))
 
-    print("\nlabels (%s): %s" % (len(labels), labels))
-    labels = torch.tensor(labels).long()
-    print("labels (%s): %s" % (labels.size(), labels))
+    train_labels = torch.tensor(train_labels).long()
+    print("train_labels (%s): %s" % (train_labels.size(), train_labels))
 
     '''
-    # Filter data by label: labels == 2 will return a tensor with True/False depending on the label for each sample
-    # this True/False tensor can be used to index trainig_data, returning only the ones for which the condition was True
-    twos = training_data[labels == 2]
+    # Filter training data by label: train_labels == 2 will return a tensor with True/False depending on the label for each sample
+    # this True/False tensor can be used to index train_data, returning only the ones for which the condition was True
+    twos = train_data[train_labels == 2]
     print("twos (%s): %s" % (twos.size(), twos))
     # show the first "2" on the screen
     lab3.show_image(twos[0], scale=lab3.SCALE_01)
     '''
-
-    model = TwoLayerNN(28*28, 40, 10, nn.ReLU())
-    optimizer = optim.Adam(model.parameters(), lr=1e-1)
+    # NOTE FOR LAB: 30 neurons work very well
+    model = TwoLayerNN(28*28, 30, 10, nn.ReLU())
+    # NOTE FOR LAB: 1e-2 works better than 1e-2
+    optimizer = optim.Adam(model.parameters(), lr=1e-2)
     loss_fn = nn.CrossEntropyLoss()
     print("\nmodel: %s" % model)
 
-    train_nn(1000, model, optimizer, loss_fn, training_data, labels, 28*28, 10)
-    y_pred = model(training_data.view(-1, 28*28))
-    print("\ny_predic (%s): %s ..." % (y_pred.size(), y_pred[:10, :]))
-    predictions = y_pred.max(1).indices
+    train_nn(1000, model, optimizer, loss_fn, train_data, train_labels, 28*28, 10)
+    train_y_pred = model(train_data)
+    train_predictions = train_y_pred.max(1).indices
 
-    print("\nlabels (%s): %s" % (len(labels), labels))
-    print("predictions (%s): %s" % (len(predictions), predictions))
+    print("\ntrain_y_pred (%s): %s ..." % (train_y_pred.size(), train_y_pred[:10, :]))
+    print("\ntrain_labels (%s): %s" % (len(train_labels), train_labels))
+    print("train_predic (%s): %s" % (len(train_predictions), train_predictions))
+
+    # --------------- VALIDATION ---------------
+    # read test data
+    test_data, test_data_dims = read_idx.read(testdataf, 50)
+    test_labels, test_label_dims = read_idx.read(testlabelf, 50)
+
+    # convert tensors to the appropriate data types, also shape and normalize images data
+    test_data = torch.tensor(test_data, dtype=torch.float)
+    test_data = test_data.view((-1, 28 * 28))
+    # normalize training data
+    test_data /= 255
+    print("'\ntest_data (%s): %s" % (test_data.size(), test_data))
+
+    test_labels = torch.tensor(test_labels).long()
+    print("test_labels (%s): %s" % (test_labels.size(), test_labels))
+
+    # predict test labels using trained model
+    test_y_pred = model(test_data)
+    test_predictions = test_y_pred.max(1).indices
+
+    print("\ntest_y_pred (%s): %s ..." % (test_y_pred.size(), test_y_pred[:10, :]))
+    print("\ntest_labels (%s): %s" % (len(test_labels), test_labels))
+    print("test_predic (%s): %s" % (len(test_predictions), test_predictions))
 
 
 if __name__ == "__main__":
