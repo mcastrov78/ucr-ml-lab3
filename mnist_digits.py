5f11dc23d47712c5590ccf5043c55e117e18424e
Mauricio Castro <mauricio.castro@avantica.net>
2020-05-24 09:28:04 -0600

    Using Sigmoid and ReLU.

--- mnist_digits.py
+++ mnist_digits.py
@@ -1,4 +1,5 @@
 import read_idx
+import lab3
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -54,10 +55,9 @@ def train_nn(iterations, nn_model, optimizer, nn_loss_fn, tensor_x, tensor_y, in
     :return:
     """
     print("\n*** TRAINING NN ***")
-    print("\ntensor_x (%s): %s" % (tensor_x.shape, tensor_x))
-    print("\ntensor_y (%s): %s" % (tensor_y.shape, tensor_y))
     tensor_x_reshaped = tensor_x.view(-1, input_n)
-    print("\ntensor_x_reshaped (%s): %s" % (tensor_x_reshaped.shape, tensor_x_reshaped))
+    #print("\ntensor_x_reshaped (%s): %s" % (tensor_x_reshaped.shape, tensor_x_reshaped))
+    #print("\ntensor_y (%s): %s" % (tensor_y.shape, tensor_y))
 
     for it in range(1, iterations + 1):
         tensor_y_pred = nn_model(tensor_x_reshaped)
@@ -80,20 +80,19 @@ def get_images_and_labels_tensors(images_filename, labels_filename):
     images_tensor = images_tensor.view((-1, 28 * 28))
     images_tensor /= 255
 
-    print("images_tensor (%s): %s" % (images_tensor.size(), images_tensor))
     labels_tensor = torch.tensor(labels_tensor).long()
-    print("labels_tensor (%s): %s" % (labels_tensor.size(), labels_tensor))
+    #print("images_tensor (%s): %s" % (images_tensor.size(), images_tensor))
+    #print("labels_tensor (%s): %s" % (labels_tensor.size(), labels_tensor))
 
     return images_tensor, labels_tensor
 
 
-def train(trainingdataf, traininglabelf, model):
-    print("\n--------------- TRAINING - --------------\n")
+def train(trainingdataf, traininglabelf, model, learning_rate):
+    print("\n--------------- TRAINING - --------------")
     # read training data
     train_data, train_labels = get_images_and_labels_tensors(trainingdataf, traininglabelf)
 
-    # NOTE FOR LAB: 1e-3 works better than 1e-2 and 1e-2
-    optimizer = optim.Adam(model.parameters(), lr=1e-3)
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
     loss_fn = nn.CrossEntropyLoss()
     print("\nmodel: %s" % model)
 
@@ -101,7 +100,7 @@ def train(trainingdataf, traininglabelf, model):
     train_y_pred = model(train_data)
     train_predictions = train_y_pred.max(1).indices
 
-    print("\ntrain_y_pred (%s): %s ..." % (train_y_pred.size(), train_y_pred[:10, :]))
+    #print("\ntrain_y_pred (%s): %s ..." % (train_y_pred.size(), train_y_pred[:10, :]))
     print("\ntrain_labels (%s): %s ..." % (len(train_labels), train_labels[:100]))
     print("train_predic (%s): %s ..." % (len(train_predictions), train_predictions[:100]))
 
@@ -123,7 +122,7 @@ def generate_classification_report(test_labels, test_predictions):
 
 
 def validate(testdataf, testlabelf, model):
-    print("\n--------------- VALIDATION - --------------\n")
+    print("\n--------------- VALIDATION - --------------")
     # read test data
     test_data, test_labels = get_images_and_labels_tensors(testdataf, testlabelf)
 
@@ -131,7 +130,7 @@ def validate(testdataf, testlabelf, model):
     test_y_pred = model(test_data)
     test_predictions = test_y_pred.max(1).indices
 
-    print("\ntest_y_pred (%s): %s ..." % (test_y_pred.size(), test_y_pred[:10, :]))
+    #print("\ntest_y_pred (%s): %s ..." % (test_y_pred.size(), test_y_pred[:10, :]))
     print("\ntest_labels (%s): %s ..." % (len(test_labels), test_labels[:100]))
     print("test_predic (%s): %s ..." % (len(test_predictions), test_predictions[:100]))
 
@@ -141,12 +140,27 @@ def validate(testdataf, testlabelf, model):
 def main(trainingdataf="train-images.idx3-ubyte", traininglabelf="train-labels.idx1-ubyte",
          testdataf="t10k-images.idx3-ubyte", testlabelf="t10k-labels.idx1-ubyte"):
 
-    # --------------- TRAINING - --------------
-    # NOTE FOR LAB: 30 neurons work very well
-    model = TwoLayerNN(28 * 28, 50, 10, nn.ReLU())
-    train(trainingdataf, traininglabelf, model)
+    # --------------- TRAINING ---------------
+    # NOTE FOR LAB: 50 neurons works the best. More than that doesn't really improve.
+    # NOTE FOR LAB: 1e-3 works better than 1e-2 and 1e-2. 1e-4 can cause errors
+    model = TwoLayerNN(28 * 28, 50, 10, nn.Sigmoid())
+    train(trainingdataf, traininglabelf, model, 1e-3)
     validate(testdataf, testlabelf, model)
 
-    
+    print("model: %s" % model)
+    print("\nPARAMETERS")
+    for name, param, in model.named_parameters():
+        print("\nname: %s" % name)
+        print("tensor(%s): %s" % (param.size(), param.data))
+        if name == "hidden_linear.weight":
+            lab3.show_image(param.data[0], scale=lab3.SCALE_01)
+
+    # --------------- TRAINING ---------------
+    # NOTE FOR LAB: 30 neurons work very well
+    #model = TwoLayerNN(28 * 28, 50, 10, nn.ReLU())
+    #train(trainingdataf, traininglabelf, model, 1e-3)
+    #validate(testdataf, testlabelf, model)
+
+
 if __name__ == "__main__":
     main()
